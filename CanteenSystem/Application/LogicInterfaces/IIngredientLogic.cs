e0db0aeb43a1e8ba3d41c0742556574b21fea87e
VeronicaTverdohleb <101176218+VeronicaTverdohleb@users.noreply.github.com>
2023-05-08 13:07:24 +0200

    commit to pull

--- CanteenSystem/Application/LogicInterfaces/IIngredientLogic.cs
+++ CanteenSystem/Application/LogicInterfaces/IIngredientLogic.cs
@@ -1,6 +1,14 @@
-﻿namespace Application.LogicInterfaces;
+﻿using Shared.Dtos;
+using Shared.Model;
+
+namespace Application.LogicInterfaces;
 
 public interface IIngredientLogic
 {
-    
+    Task<Ingredient> CreateAsync(IngredientCreationDto dto);
+    Task UpdateIngredientAmount(IngredientUpdateDto dto);
+    Task<IEnumerable<Ingredient>> GetAsync();
+    Task<IngredientBasicDto?> GetByIdAsync(int id);
+    Task<IngredientBasicDto?> GetByNameAsync(string name);
+    Task DeleteIngredient(int id);
 }
