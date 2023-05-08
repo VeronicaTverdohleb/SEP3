a841fcde6928412436b3f5c16ac975ca29f80279
The2InchSpecial <tcbcedric@hotmail.com>
2023-05-08 10:31:05 +0200

    pushing to avoid conflict

---
+++ CanteenSystem/Shared/Dtos/OrderBasicDto.cs
@@ -0,0 +1,17 @@
+﻿using Shared.Model;
+
+namespace Shared.Dtos;
+
+public class OrderBasicDto
+{
+    public int Id { get; }
+    public string CustomerName { get; }
+    public IEnumerable<Item> Items { get; }
+
+    public OrderBasicDto(int id, string customerName, IEnumerable<Item> items)
+    {
+        Id = id;
+        CustomerName = customerName;
+        Items = items;
+    }
+}
