7d01660596923085f9d351bd020dfe2cb5b6a0af
PetraHrabakova <petrahrab3@gmail.com>
2023-05-09 13:08:03 +0200

    SeeManu adjustments

--- CanteenSystem/DataAccess/DAOs/MenuDao.cs
+++ CanteenSystem/DataAccess/DAOs/MenuDao.cs
@@ -15,14 +15,30 @@ public class MenuDao : IMenuDao
     
     public async Task<Menu> GetMenuByDateAsync(DateTime date)
     {
-        String newDate = date.Year + "-" + date.Month + "-" + date.Day;
+        // We need format yyyy-mm-dd, but the DateTime creates yyyy-m-d when the Month/Date are only one digit
+        String newDate = ConvertDate(date);
 
-        return null;
-        /***
-        Menu? found = await context.Menus
-            .AsNoTracking()
-            .SingleOrDefaultAsync(menu => menu.Date.Equals(newDate));
-        return found;
-        ***/
+        IQueryable<Item>? found = context.Items
+            .FromSql($"Select * from Items where MenuDate = {newDate}");
+        
+        if (!found.Any())
+            Console.WriteLine($"There are no Items on this date");
+
+        ICollection<Item> items = found.ToList();
+        Menu menu = new Menu(items, date);
+
+        return menu;
+    }
+
+    private static string ConvertDate(DateTime date)
+    {
+        String newDate;
+        if (date.Day < 10)
+            newDate = date.Year + "-" + date.Month + "-0" + date.Day;
+        else if (date.Month < 10)
+            newDate = date.Year + "-0" + date.Month + "-" + date.Day;
+        else
+            newDate = date.Year + "-" + date.Month + "-" + date.Day;
+        return newDate;
     }
 }
--- CanteenSystem/HttpClients/Implementations/MenuHttpClient.cs
+++ CanteenSystem/HttpClients/Implementations/MenuHttpClient.cs
@@ -17,7 +17,7 @@ public class MenuHttpClient : IMenuService
 
     public async Task<MenuBasicDto> GetMenuByDateAsync(DateTime date)
     {
-        string query = ConstructQuery(date);
+        string query = ConstructQuery(ConvertDate(date));
         HttpResponseMessage response = await client.GetAsync("/GetMenu" + query);
         string result = await response.Content.ReadAsStringAsync();
         if (!response.IsSuccessStatusCode)
@@ -32,10 +32,24 @@ public class MenuHttpClient : IMenuService
         return menu;
     }
     
-    private static string ConstructQuery(DateTime date)
+    private static string ConstructQuery(String date)
     {
         return $"?date={date}";
     }
+    
+    private static string ConvertDate(DateTime date)
+    {
+        String newDate;
+        if (date.Day < 10)
+            newDate = date.Year + "-" + date.Month + "-0" + date.Day;
+        else if (date.Month < 10)
+            newDate = date.Year + "-0" + date.Month + "-" + date.Day;
+        else
+            newDate = date.Year + "-" + date.Month + "-" + date.Day;
+        return newDate;
+    }
+    
+    
     
     
 }
