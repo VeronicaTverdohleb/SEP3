0ec570759a03ffa223a793b2b8ba5ec6ae1dd40b
PetraHrabakova <petrahrab3@gmail.com>
2023-05-10 11:55:22 +0200

    SeeMenu

--- CanteenSystem/HttpClients/Implementations/MenuHttpClient.cs
+++ CanteenSystem/HttpClients/Implementations/MenuHttpClient.cs
@@ -17,7 +17,7 @@ public class MenuHttpClient : IMenuService
 
     public async Task<MenuBasicDto> GetMenuByDateAsync(DateTime date)
     {
-        string query = ConstructQuery(date);
+        string query = ConstructQuery(ConvertDate(date));
         HttpResponseMessage response = await client.GetAsync("/GetMenu" + query);
         string result = await response.Content.ReadAsStringAsync();
         if (!response.IsSuccessStatusCode)
@@ -32,10 +32,26 @@ public class MenuHttpClient : IMenuService
         return menu;
     }
     
-    private static string ConstructQuery(DateTime date)
+    private static string ConstructQuery(String date)
     {
         return $"?date={date}";
     }
+    
+    private static string ConvertDate(DateTime date)
+    {
+        String newDate;
+        if (date.Month < 10 && date.Day < 10)
+            newDate = date.Year + "-0" + date.Month + "-0" + date.Day;
+        else if (date.Month >= 10 && date.Day < 10)
+            newDate = date.Year + "-" + date.Month + "-0" + date.Day;
+        else if (date.Month < 10 && date.Day >= 10)
+            newDate = date.Year + "-0" + date.Month + "-" + date.Day;
+        else
+            newDate = date.Year + "-" + date.Month + "-" + date.Day;
+        return newDate;
+    }
+    
+    
     
     
 }
