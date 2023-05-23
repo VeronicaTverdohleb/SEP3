9a72104afa48408c84f5fd3490a99d2a4b24c2f1
The2InchSpecial <tcbcedric@hotmail.com>
2023-05-23 16:35:52 +0200

    documentation for orderlogic

--- CanteenSystem/Application/Logic/OrderLogic.cs
+++ CanteenSystem/Application/Logic/OrderLogic.cs
@@ -18,11 +18,22 @@ public class OrderLogic : IOrderLogic
         this.userDao = userDao;
     }
 
+    /// <summary>
+    /// Gets a IEnumerable of orders corresponding to the chosen search parameters
+    /// </summary>
+    /// <param name="parameters">Dto containing the username, date, order status and order number to filter by</param>
+    /// <returns>IEnumerable of orders corresponding to the chosen search parameters</returns>
     public Task<IEnumerable<Order>> GetAllOrdersAsync(SearchOrderParametersDto parameters)
     {
         return orderDao.GetAllOrdersAsync(parameters);
     }
 
+    /// <summary>
+    /// gets a specific order by its unique id
+    /// </summary>
+    /// <param name="id">id of the order</param>
+    /// <returns>The specified order</returns>
+    /// <exception cref="Exception">exception if no order is found with the chosen id</exception>
     public async Task<Order> GetOrderByIdAsync(int id)
     {
         Order? order = await orderDao.GetByIdAsync(id);
@@ -34,6 +45,11 @@ public class OrderLogic : IOrderLogic
         return order;
     }
 
+    /// <summary>
+    /// Deletes an order chosen by its id
+    /// </summary>
+    /// <param name="id">the id of the order that is to be deleted</param>
+    /// <exception cref="Exception">exception if no order with the chosen id is found</exception>
     public async Task DeleteOrderAsync(int id)
     {
         Order? order = await orderDao.GetByIdAsync(id);
@@ -45,6 +61,12 @@ public class OrderLogic : IOrderLogic
         await orderDao.DeleteOrderAsync(id);
     }
     
+    /// <summary>
+    /// Creates an order
+    /// </summary>
+    /// <param name="dto">dto containing the necessary information to create an order</param>
+    /// <returns>The created order</returns>
+    /// <exception cref="Exception">Exceptions for when items that don't exist are part of the dto or if the dto has no items</exception>
     public async Task<Order> CreateOrderAsync(MakeOrderDto dto)
     {
         foreach (int item in dto.ItemIds)
@@ -65,6 +87,13 @@ public class OrderLogic : IOrderLogic
         
     }
 
+    /// <summary>
+    /// Updates an existing order with new information
+    /// </summary>
+    /// <param name="dto">dto holding the information that can be changed</param>
+    /// <exception cref="Exception">exceptions for when the specified order does not exist,
+    /// when changes are attempted to be made on an order that is "ready for pickup"
+    /// or to prevent updating the order with an empty list of items</exception>
     public async Task UpdateOrderAsync(OrderUpdateDto dto)
     {
         Order? existing = await orderDao.GetByIdAsync(dto.Id);
